4190fbad196eaf5079ee42032a063ffa68648760
jordi watts <jordiwatts@gmail.com>
2022-02-06 01:28:58 +0000

    vpc #4

--- vpc.tf
+++ vpc.tf
@@ -1,15 +1,17 @@
-module "network" {
+module "vpc" {
   source  = "app.terraform.io/jordiwatts-cdw/network/google"
   version = "3.4.0"
 
-    project_id   = "var.project"
-    network_name = "gaurav-network"
+  project_id   = "var.project"
+  network_name = "gaurav-network"
+  subnets = "10.100.10.0/24"
+}
 
-    subnets = [
-    {
-        subnet_name   = "gaurav-subnet"
-        subnet_ip     = "10.100.10.0/24"
-        subnet_region = var.region
-    }
-  ]
+module "network" {
+  source  = "app.terraform.io/jordiwatts-cdw/network/google"
+  version = "4.1.0"
+
+  network_name = "gaurav-network"
+  project_id = "var.project"
+  subnets = "10.100.10.0/24"
 }
